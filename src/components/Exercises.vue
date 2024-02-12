88c0b2eb1321990ab06b17f61fc00f1f60b59e42
Chris <chrisbrowne91@gmail.com>
2024-02-12 07:18:03 -0500

    Exercise component done

--- src/components/Exercises.vue
+++ src/components/Exercises.vue
@@ -1,6 +1,88 @@
 <template>
-  <p class="text-celtic">EXERCISE VIDEOS -</p>
+  <div
+    class="lg:grid lg:grid-rows-3 lg:grid-cols-3 lg:gap-y-12 lg:items-center lg:py-12 lg:place-items-center flex flex-wrap justify-center text-center"
+  >
+    <p class="text-celtic text-5xl"><b>EXERCISE VIDEOS -</b></p>
+    <iframe
+      width="360"
+      height="206"
+      src="https://www.youtube.com/embed/FRDGlIJTVt8?si=KjWxpSSOUR1VsPlG"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+    <iframe
+      width="360"
+      height="206"
+      src="https://www.youtube.com/embed/FRDGlIJTVt8?si=KjWxpSSOUR1VsPlG"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+    <iframe
+      width="360"
+      height="206"
+      src="https://www.youtube.com/embed/FRDGlIJTVt8?si=KjWxpSSOUR1VsPlG"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe
+    ><iframe
+      width="360"
+      height="206"
+      src="https://www.youtube.com/embed/FRDGlIJTVt8?si=KjWxpSSOUR1VsPlG"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe
+    ><iframe
+      width="360"
+      height="206"
+      src="https://www.youtube.com/embed/FRDGlIJTVt8?si=KjWxpSSOUR1VsPlG"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+    <iframe
+      width="360"
+      height="206"
+      src="https://www.youtube.com/embed/FRDGlIJTVt8?si=KjWxpSSOUR1VsPlG"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe
+    ><iframe
+      width="360"
+      height="206"
+      src="https://www.youtube.com/embed/FRDGlIJTVt8?si=KjWxpSSOUR1VsPlG"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe
+    ><iframe
+      width="360"
+      height="206"
+      src="https://www.youtube.com/embed/FRDGlIJTVt8?si=KjWxpSSOUR1VsPlG"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  </div>
 </template>
 
 <script setup></script>
-<style></style>
+
+<style>
+iframe {
+  max-width: 100%;
+  height: auto;
+}
+</style>
